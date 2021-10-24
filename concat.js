a0c7a28f9f78c326e2c7c1a412cc3d8782e892b8
Hailan Wang <hailan6257@email.com>
2021-10-24 23:18:13 +0000

    A function that concats two arrays

---
+++ concat.js
@@ -0,0 +1,15 @@
+function concat(array1, array2){
+  if(array2.length != 0){
+    for(let element of array2){
+      array1.push(element);
+    }
+    return array1;
+  }else{
+    return array1;
+  }
+}
+
+console.log(concat([ 1, 2, 3 ], [ 4, 5, 6 ]), "=?", [ 1, 2, 3, 4, 5, 6 ]);
+console.log(concat([ 0, 3, 1 ], [ 9, 7, 2 ]), "=?", [ 0, 3, 1, 9, 7, 2 ]);
+console.log(concat([], [ 9, 7, 2 ]), "=?", [ 9, 7, 2 ]);
+console.log(concat([ 5, 10 ], []), "=?", [ 5, 10 ]);
